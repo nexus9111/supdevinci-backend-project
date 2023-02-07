da27b4a4ebc60615bb2da07e4b6379fbd689deff
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2023-02-07 10:03:28 +0100

    feat: update readme, update install script, add tests

---
+++ tests/main.test.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+const mongo = require("../config/mongo");
+const app = require("../app");
+
+
+describe("Test the root path", () => {
+    beforeAll(() => {
+        mongo.connect();
+    });
+
+    test("🧪 Server should be", async () => {
+        const response = await request(app).get("/");
+        expect(response.statusCode).toBe(200);
+    });
+
+    test("🧪 No blogs should exists at init", async () => {
+        const response = await request(app).get("/blogs");
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty("success");
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data.articles.length).toBe(0);
+    });
+});
